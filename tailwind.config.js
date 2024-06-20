5bdc3b5bf0389c88928e394116954f18b3ffffb5
Naman Tripathi <namantripathi937@gmail.com>
2024-06-20 16:22:54 +0530

    outsite the src update

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily:{
+        playwrite: "Playwrite NZ",
+      },
+      colors:{
+        naman : '#1b1b1b',
+      }
+    },
+  },
+  plugins: [],
+}
+
